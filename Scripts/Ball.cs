20d249b61cc93cd162bed1a38741af56d04ddb89
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2023-04-27 06:04:27 -0500

    Add files via upload

---
+++ Scripts/Ball.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // property with private setter defaulted to "Unnamed"
+    
+    // private Color variable 
+    
+
+    // a constructor that logs out the name of the ball
+        // should show "Unnamed" as is the default until a name is given
+    
+
+    // public method that sets the name of the Ball instance
+        // Logs out the name
+
+
+    // public method that sets the color of the Ball instance
+
+}
---
+++ Scripts/GameController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    // public reference to the prefab we will be spawning
+    
+    // 3 references to the 3 instances we will spawn
+    
+
+    // 3 Color variables (red, blue, green) that we will set instance to 
+
+
+    // 3 positions for each of the instances
+
+
+    
+    void Start()
+    {
+        // instantiate the prefab
+        
+        // set the position of the instance
+        
+        // get an instance to the Ball component
+        
+        // set the name of the instance to correspond to the color it will be
+        
+        // set the color of the object by calling it's public method to set color
+        
+
+
+        // repeat the above for the other 2 object instances
+
+
+
+    }
+
+
+}
